a031f398b3ef531d9401435979e9fe8f6b3a2057
Wade-Martin <wsmartin23@gmail.com>
2019-07-15 11:22:37 +1000

    added basic logic to newBusiness component

--- src/components/Admin/NewBusiness.js
+++ src/components/Admin/NewBusiness.js
@@ -1,9 +1,36 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { async } from 'q';
 
 
 export default function NewBusiness() {
-  return (
+  const [businessID, setBusinessID] = useState('');
+  const [businessName, setBusinessName] = useState('');
+  const [abn, setAbn] = useState('');
 
+  const handleSubmit = (e) => {
+    console.log(businessID);
+    console.log(businessName);
+    console.log(abn);
+    e.preventDefault();
+    createBusiness(businessID, businessName, abn);
+  }
+
+  const createBusiness = async (businessID, businessName, abn) => {
+    const data = { businessID, businessName, abn }
+    let response;
+    try {
+      response = await axios.post(process.env.REACT_APP_API_URL + '/admin/newBusiness', data);
+    }
+    catch (error) {
+      console.log(error);
+    }
+    finally {
+      console.log(response);
+    }
+  }
+
+  return (
+    <p>Add a new Business</p>
   )
 }
