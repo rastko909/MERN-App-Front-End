975af7b99e5b4308265b7cfedf25198e4e43c8aa
Wade-Martin <wsmartin23@gmail.com>
2019-07-19 13:32:35 +1000

    claim id reference fix

--- src/Claim/Form/index.js
+++ src/Claim/Form/index.js
@@ -68,7 +68,7 @@ class Form extends React.Component {
 
   sendAnswers = async () => {
     const response = await axios.post(process.env.REACT_APP_API_URL + "/claim/new", this.state.newClaim);
-    let claimId = response.data.id;
+    let claimId = response.data.claimId;
     console.log(claimId)
     const { business_id } = this.state.newClaim;
 
