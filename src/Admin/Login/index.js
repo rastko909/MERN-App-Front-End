a4085b7ae9bbe280e88fedf1ba2757224c2f1c97
Timothy Waldron <tim@waldron.im>
2019-07-22 13:07:58 +1000

    Adjusted login post request

--- src/Admin/Login/index.js
+++ src/Admin/Login/index.js
@@ -58,7 +58,7 @@ export default function SignIn(props) {
     const data = { email, password }
     let response;
     try {
-      response = await axios.post(process.env.REACT_APP_API_URL + '/admin/login', data);
+      response = await axios.post(process.env.REACT_APP_API_URL + '/admin/login/', data);
     }
     catch (error) {
       console.log('Error in catch: ', error.message);
@@ -89,7 +89,7 @@ export default function SignIn(props) {
         <Typography component="h1" variant="h5">
           Admin Portal
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} method="post" noValidate>
           <TextField
             variant="outlined"
             margin="normal"
